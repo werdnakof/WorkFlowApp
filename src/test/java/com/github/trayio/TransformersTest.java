4d3dddc2ddbf6e37164631a1449c5809aeda7488
werdnakof <andrewfok007@gmail.com>
2018-07-14 11:53:52 +0100

    fixed tests

--- src/test/java/com/github/trayio/TransformersTest.java
+++ src/test/java/com/github/trayio/TransformersTest.java
@@ -1,5 +1,6 @@
 package com.github.trayio;
 
+import io.reactivex.functions.Predicate;
 import io.reactivex.observers.TestObserver;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,19 @@ import static org.junit.Assert.assertEquals;
 
 public class TransformersTest {
 
+    private Task task1;
+    private Task task2;
+    List<Task> steps;
+    private WorkFlow wf;
+
+    @Before
+    public void setTestData() {
+        task1 = new Task(0, "step0");
+        task2 = new Task(1, "step1");
+        steps = Arrays.asList(task1, task2);
+        wf = new WorkFlow("test", steps);
+    }
+
     @Before
     public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
         Field instance = DBService.class.getDeclaredField("instance");
@@ -21,10 +35,11 @@ public class TransformersTest {
 
     @Test
     public void test_getWorkFlow() {
-        Observable<ConsoleEvent> ob = Observable.just(new ConsoleEvent("test.json"));
-        List<Integer> steps = Arrays.asList(0, 1, 2);
-        WorkFlow expected = new WorkFlow(0,3, steps);
-        TestObserver<WorkFlow> testObserver = ob.compose(getWorkFlow()).test();
+        WorkFlow expected = wf;
+
+        TestObserver<WorkFlow> testObserver = Observable.just(new ConsoleEvent("test.json"))
+                .compose(getWorkFlow())
+                .test();
 
         testObserver.awaitTerminalEvent();
         testObserver.assertValue(expected);
@@ -32,63 +47,85 @@ public class TransformersTest {
 
     @Test
     public void test_saveWorkFlow() {
-        WorkFlow expected = new WorkFlow(0, 3, Arrays.asList(0, 1, 2));
+        WorkFlow expected = wf;
+        expected.setId(0);
 
-        TestObserver<WorkFlow> testObserver = Observable.just(expected)
+        TestObserver<WorkFlow> testOb = Observable.just(expected)
                 .compose(saveWorkFlow())
                 .test();
 
-        testObserver.awaitTerminalEvent();
-        testObserver.assertValue(expected);
+        testOb.awaitTerminalEvent();
+        testOb.assertValue(expected);
 
         assertEquals(expected, DBService.getInstance().getWorkFlow(0));
     }
 
     @Test
-    public void test_saveWorkFlowExecution_1() {
-        List<Integer> steps = Arrays.asList(1, 2, 3);
-        WorkFlow wf = new WorkFlow(0, 3, steps);
+    public void test_saveWorkFlowExecution() {
+        Task task4 = new Task(4, "step4");
 
-        WorkFlowExecution wfe1 = new WorkFlowExecution(wf, 0);
+        WorkFlowExecution wfe1 = new WorkFlowExecution(wf, task1);
+        WorkFlowExecution wfe2 = new WorkFlowExecution(wf, task2);
+        WorkFlowExecution wfe3 = new WorkFlowExecution(wf, task4);
 
-        TestObserver<WorkFlowExecution> wfes1 = Observable.just(wfe1)
+        WorkFlowExecution expected1 = new WorkFlowExecution(0, wf, task1);
+        WorkFlowExecution expected2 = new WorkFlowExecution(1, wf, task2);
+        WorkFlowExecution expected3 = new WorkFlowExecution(2, wf, task4);
+
+        TestObserver<WorkFlowExecution> testOb = Observable.fromArray(wfe1, wfe2, wfe3)
                 .compose(saveWorkFlowExecution())
                 .test();
 
-        wfes1.awaitTerminalEvent();
-        wfes1.assertValue(wfe1);
+        testOb.awaitTerminalEvent();
 
-        assertEquals(wfe1, DBService.getInstance().getWorkFlowExecution(0));
-
-        WorkFlowExecution wfe2 = new WorkFlowExecution(wf, 1);
-        TestObserver<WorkFlowExecution> wfes2 = Observable.just(wfe2)
-                .compose(saveWorkFlowExecution())
-                .test();
-
-        wfes2.awaitTerminalEvent();
-        wfes2.assertValue(wfe2);
-        assertEquals(wfe2, DBService.getInstance().getWorkFlowExecution(1));
+        assertEquals(expected1, DBService.getInstance().getWorkFlowExecution(0));
+        assertEquals(expected2, DBService.getInstance().getWorkFlowExecution(1));
+        assertEquals(expected3, DBService.getInstance().getWorkFlowExecution(2));
     }
 
     @Test
-    public void test_saveWorkFlowExecution_2() {
-        List<Integer> steps = Arrays.asList(1, 2, 3);
-        WorkFlow wf = new WorkFlow(1, 3, steps);
+    public void test_execute() {
 
-        WorkFlowExecution wfe1 = new WorkFlowExecution(0, wf, 0);
-        WorkFlowExecution wfe2 = new WorkFlowExecution(1, wf, 1);
-        WorkFlowExecution wfe3 = new WorkFlowExecution(2, wf, 2);
+        WorkFlowExecution wfe1 = new WorkFlowExecution(10, wf, task1);
+        WorkFlowExecution wfe2 = new WorkFlowExecution(11, wf, new Task(4, "force-error"));
 
-        TestObserver<WorkFlowExecution> wfes = Observable.fromIterable(Arrays.asList(0, 1, 2))
-                .concatMap(num -> Observable.just(new WorkFlowExecution(wf, num))
-                        .compose(saveWorkFlowExecution())).test();
+        TestObserver<Executor.Response> testOb = Observable
+                .just(wfe1, wfe2)
+                .compose(execute())
+                .test();
 
-        wfes.awaitTerminalEvent();
-        wfes.assertValueCount(3);
-        wfes.assertValues(wfe1, wfe2, wfe3);
+        testOb.awaitTerminalEvent();
 
-        assertEquals(wfe1, DBService.getInstance().getWorkFlowExecution(0));
-        assertEquals(wfe2, DBService.getInstance().getWorkFlowExecution(1));
-        assertEquals(wfe3, DBService.getInstance().getWorkFlowExecution(2));
+        Executor.Response expected1 = new Executor.Response(10, Executor.Status.SUCCESS);
+        Executor.Response expected2 = new Executor.Response(11, Executor.Status.FAILURE);
+
+        testOb.assertValueCount(2)
+                .assertValueAt(0, response -> response.equals(expected1))
+                .assertValueAt(1, response -> response.equals(expected2));
+    }
+
+    @Test
+    public void test_updateWorkFlowExecution() {
+        WorkFlowExecution wfe1 = new WorkFlowExecution(0, wf, task1);
+
+        DBService.getInstance().write(wfe1);
+        assertEquals(WorkFlowExecution.State.PENGING, DBService.getInstance().getWorkFlowExecution(0).getState());
+
+        TestObserver<Executor.Response> testOb = Observable
+                .just(new Executor.Response(0, Executor.Status.SUCCESS))
+                .compose(updateWorkFlowExecution())
+                .test();
+
+        testOb.awaitTerminalEvent();
+
+        assertEquals(WorkFlowExecution.State.COMPLETED, DBService.getInstance().getWorkFlowExecution(0).getState());
+
+        testOb = Observable
+                .just(new Executor.Response(0, Executor.Status.FAILURE))
+                .compose(updateWorkFlowExecution())
+                .test();
+
+        testOb.awaitTerminalEvent();
+        assertEquals(WorkFlowExecution.State.ERROR, DBService.getInstance().getWorkFlowExecution(0).getState());
     }
 }
